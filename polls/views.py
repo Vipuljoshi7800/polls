982ee0d92ffaa3e275a7d88ea8a3f3904b65ed56
Vipul <vipuljoshi7800@gmail.com>
2019-10-14 16:06:21 +0530

    addedf pagination

--- polls/views.py
+++ polls/views.py
@@ -8,7 +8,8 @@ from polls.form import PostForm
 from polls import form
 from polls.models import Post
 from django.utils import timezone
-
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
 
 def post_create(request):
   if request.method == "POST":
@@ -22,9 +23,57 @@ def post_create(request):
       return render(request, 'post/create.html', {'form': form})
   
 def post_index(request):
-  all = Post.objects.all()
-  return render(request,'post/index.html',{'all':all})
+  post_list = Post.objects.all()
+  paginator = Paginator(post_list, 4)
+  
+  try:
+    page_number = int(request.GET.get('page', '1'))
+  except:
+    page_number = 1
+  
+  try:
+    # get data list for the specified page_number.
+    posts = paginator.page(page_number)
+  except PageNotAnInteger:
+    # if the page_number is not an integer then return the first page data.
+    posts = paginator.page(1)
+  except EmptyPage:
+    # get the lat page data if the page_number is bigger than last page number.
+    posts = paginator.page(paginator.num_pages)
 
+  base_url = '/post?' 
+  return render(request, 'post/index.html', {'post_list': posts, 'paginator' : paginator, 'base_url': base_url})
+
+  # contacts = paginator.get_page(page)
+  # return render(request, 'post/index.html', {'contacts': contacts})
+
+  # page_number = request.GET.get('page', 3)
+  # page = request.Ge
+  # paginate_result = Paginator(user_list, page_number)
+  # user_list = paginate_result(1)
+  # paginator = paginate_result(4)
+   
+  # return render(request, 'post/index.html',{'user_list': user_list, 'paginator' : paginator})
+ 
+  # all = Post.objects.all()
+  # return render(request,'post/index.html',{'all':all})
+
+def do_paginate(data_list, page_number):
+    ret_data_list = data_list
+    # suppose we display at most 2 records in each page.
+    result_per_page = 2
+    # build the paginator object.
+    paginator = Paginator(data_list, result_per_page)
+    try:
+        # get data list for the specified page_number.
+        ret_data_list = paginator.page(page_number)
+    except EmptyPage:
+        # get the lat page data if the page_number is bigger than last page number.
+        ret_data_list = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # if the page_number is not an integer then return the first page data.
+        ret_data_list = paginator.page(1)
+    return [ret_data_list, paginator]
 
 def post_detail(request, pk):
   post = get_object_or_404(Post, pk=pk)
@@ -42,3 +91,4 @@ def post_edit(request, pk):
   else:
     form = PostForm(instance=post)
   return render(request, 'post_edit.html', {'form': form})
+
